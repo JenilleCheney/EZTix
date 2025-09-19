1932c3150fd72c4136dd5b53a64f531b1e2368e7
W0198745JC <w0198745@campus.nscc.ca>
2025-09-19 11:16:17 -0300

    Added properties, Navigation , primary and foreign keys

--- EZTix/Models/Category.cs
+++ EZTix/Models/Category.cs
@@ -2,5 +2,10 @@
 {
     public class Category
     {
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; } = string.Empty;
+
+        public List<Event>? Events { get; set; } 
     }
 }
--- EZTix/Models/Event.cs
+++ EZTix/Models/Event.cs
@@ -2,5 +2,22 @@
 {
     public class Event
     {
+        public int EventID { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime EventTime  { get; set; }
+
+        public string Location { get; set; } = string.Empty;
+
+        public string Owner { get; set; } = string.Empty;
+
+        public DateTime Created { get; set; }
+
+
+        public int CategoryId { get; set; }
+
+        public Category? Category { get; set; }
+
     }
 }
